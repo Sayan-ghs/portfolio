6a7e39b73e56d1f2981a6ac8608b961086fa4943
Sayan-ghs <sg865186@gmail.com>
2024-10-08 21:06:50 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/**var typed = new Typed(".autowrite", {
+    strings: ["Fronted Developer","Backend Developer", "Codder"],
+    typespeed:150,
+    backspeed:150,
+    backDelay:1000,
+    loop: true
+  });*/
